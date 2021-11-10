b1612edb69f56f2ffdc10f8acbcfc6e34b76214c
ramonfrombr <ramonfrombr@gmail.com>
2021-11-09 22:50:44 -0300

    primeiro deploy

--- aplicativo.py
+++ aplicativo.py
@@ -1,35 +1,9 @@
-from flask import (
-    Flask,
-    render_template,
-    flash,
-    redirect,
-    url_for,
-    session,
-    request,
-    logging
-)
-
-from flask_mysqldb import MySQL
+from app import create_app
 
 
-
-app = Flask(__name__)
-
-
-
-
-
-
-
-
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'oasisadmin'
-app.config['MYSQL_PASSWORD'] = 'senha'
-app.config['MYSQL_DB'] = 'OasisDB'
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-
+app = create_app()
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
